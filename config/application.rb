d08ec5a2db23b1a5a43b57d52bee0f629654bc69
Piotr <pbrych@gmail.com>
2019-03-11 18:52:48 +0100

    Remove test_unit railtie (using rspec)

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,10 @@
 require_relative 'boot'
 
 require "rails"
+require "action_controller/railtie"
+
 # Pick the frameworks you want:
+<<<<<<< Updated upstream
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -10,8 +13,17 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+=======
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
+>>>>>>> Stashed changes
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
