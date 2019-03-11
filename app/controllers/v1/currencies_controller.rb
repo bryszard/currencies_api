1712ad979c8dbdf3920802d7732544dcf48a45b8
Piotr <pbrych@gmail.com>
2019-03-11 08:44:47 +0100

    Adjust #list action in controller and corresponding spec

--- app/controllers/v1/currencies_controller.rb
+++ app/controllers/v1/currencies_controller.rb
@@ -1,8 +1,19 @@
-class V1::CurrenciesController < ApplicationController
-  def list
-    exchange_rates = Actions::FetchCurrencyData.perform
-    available_currencies = exchange_rates['rates'].keys.join(',')
+# frozen_string_literal: true
 
-    render plain: available_currencies
+class V1::CurrenciesController < ApplicationController
+  TEMPORARILY_UNAVAILABLE_MESSAGE = 'Exchange rates are temporarily not available.'
+
+  def list
+    if(current_rates = exchange_rates['rates'])
+      available_currencies = current_rates.keys.sort.reverse.join(',')
+
+      render plain: available_currencies, status: :ok
+    else
+      render plain: TEMPORARILY_UNAVAILABLE_MESSAGE, status: :unprocessable_entity
+    end
+  end
+
+  def exchange_rates
+    @_exchange_rates ||= Actions::FetchCurrencyData.perform
   end
 end
--- spec/requests/currencies_list_spec.rb
+++ spec/requests/currencies_list_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 describe 'Fetching list of currencies in descending alphabetical order', type: :request do
   subject { get '/v1/currencies' }
 
-  it 'returns a list of available currencies in a String, comma separated' do
-    expect(response.status).to eq(:ok)
+  it 'returns a list of available currencies in a String, comma separated', :vcr do
+    subject
+
+    expect(response.status).to eq(200)
     expect(response.body).to eq(
       'ZAR,USD,TRY,THB,SGD,SEK,RUB,RON,PLN,PHP,NZD,NOK,MYR,MXN,KRW,JPY,'\
       'ISK,INR,ILS,IDR,HUF,HRK,HKD,GBP,DKK,CZK,CNY,CHF,CAD,BRL,BGN,AUD'
