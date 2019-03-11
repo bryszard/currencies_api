ceb950e6640ac50c47fda684bd212faa4fd7e688
Piotr <pbrych@gmail.com>
2019-03-11 09:23:28 +0100

    Add endpoint - /currencies/show, for showing specific exchange rate

--- app/controllers/v1/currencies_controller.rb
+++ app/controllers/v1/currencies_controller.rb
@@ -2,6 +2,7 @@
 
 class V1::CurrenciesController < ApplicationController
   TEMPORARILY_UNAVAILABLE_MESSAGE = 'Exchange rates are temporarily not available.'
+  CURRENCY_NOT_FOUND = 'Given currency code has not been found.'
 
   before_action :check_availability
 
@@ -11,7 +12,11 @@ class V1::CurrenciesController < ApplicationController
     render plain: available_currencies, status: :ok
   end
 
+  def show
+    if(exchange_rate = exchange_rates.dig('rates', params.require('currency_code')))
+      render plain: exchange_rate.to_s, status: :ok
     else
+      render plain: CURRENCY_NOT_FOUND, status: :not_found
     end
   end
 
